017837efa85c51c75df495c82f5c96160e99964a
RaymondFleming <rwf5@students.uwf.edu>
2017-02-09 12:36:01 -0600

    Documented these files

--- UDPLib.c
+++ UDPLib.c
@@ -1,12 +1,17 @@
 #include "UDPLib.h"
 
-
+/*
+    This function sends a UDP message to passed hostIP at hostport of 
+    whatever is in the buffer
+*/
 int sendMessage(char *hostIP,u_short hostport,char * buffer)
 {
+    
     int sockfd;
     struct sockaddr_in dest;
-    
     sockfd=socket (AF_INET, SOCK_DGRAM,0);
+    
+    //Testing for valid socket creation
     if(sockfd<0)
     {
         perror("sendUDP:socket");
@@ -15,28 +20,29 @@ int sendMessage(char *hostIP,u_short hostport,char * buffer)
     
     struct hostent *hostptr=gethostbyname(hostIP);
 
-    
+    //Setting up socket
     memset((void *) &dest, 0, (size_t)sizeof(dest));
-    
     dest.sin_family=(short)(AF_INET);
     
     memcpy((void*)&dest.sin_addr,(void*)hostptr->h_addr,hostptr->h_length);
     dest.sin_port=htons((u_short)hostport);
     
-
-    strcpy(buffer,"test send");
-    
-    printf("\n\nSending %s\n",buffer);
-    
+    //Send UDP datagram to server
     sendto(sockfd,buffer,BUFFER_SIZE,0,
             (struct sockaddr*) &dest,(socklen_t)sizeof(dest));
             
+    //Zero the buffer to prep for next operation
     bzero(buffer,BUFFER_SIZE);
     
     close(sockfd);
     return 0;
 }
 
+
+/*
+    This function receives a UDP message on the passed port then
+    places the message in the buffer, as well as prints it to console
+*/
 int receiveMessage(u_short port,char *buffer)
 {
     int listensockfd;
@@ -48,10 +54,13 @@ int receiveMessage(u_short port,char *buffer)
     struct sockaddr_in clientAddr;
     socklen_t clientAddrLen = sizeof(clientAddr);
     
+    //Retrieve own hostname
     gethostname(hostname,32);
     
     struct hostent *hostptr=gethostbyname(hostname);
     
+    
+    //Create listen socket and test for successful creation
     listensockfd=socket (AF_INET, SOCK_DGRAM,0);
     if(listensockfd<0)
     {
@@ -59,28 +68,24 @@ int receiveMessage(u_short port,char *buffer)
         return -1;
     }
     
-    //print own hostname
-    printf("\nHostname is %s\n",hostname);
     
     //set up socket
     memset((void *) &servaddr, 0, (size_t)sizeof(servaddr));
     servaddr.sin_family=(short)(AF_INET);
     memcpy((void*)&servaddr.sin_addr,(void*)hostptr->h_addr,hostptr->h_length);
     servaddr.sin_port=htons((u_short)60002);
-    fprintf(stderr,"\nport is set up\n");
-    //bind listen socket
+    
+    //bind listen socket and test for successful bind
     bindtest=bind(listensockfd,(struct sockaddr*)&servaddr,(socklen_t)sizeof(servaddr));
     if(bindtest<0)
         fprintf(stderr,"\nError binding socket\n");
-    else
-        fprintf(stderr,"\nbound socket\n");
         
-    
-        recvfrom(listensockfd,buffer,BUFFER_SIZE,0,
-                (struct sockaddr *) &clientAddr,clientAddrLen);
-
-        printf("\nBuffer is %s\n",buffer);
+    //Receive one UDP datagram and store in buffer
+    recvfrom(listensockfd,buffer,BUFFER_SIZE,0,
+            (struct sockaddr *) &clientAddr,clientAddrLen);
+            
+    //Print received message to console
+    printf("\nReceived Message is %s\n",buffer);
                 
-    fprintf(stderr,"\nClosing socket,bytesrcvd is %d\n",bytesrcvd);
     close(listensockfd);
 }
--- UDPLib.h
+++ UDPLib.h
@@ -9,8 +9,17 @@
 #include <arpa/inet.h>
 #define BUFFER_SIZE 512
 
+
+/*
+    This function sends a UDP message to passed hostIP at hostport of 
+    whatever is in the buffer
+*/
 int sendMessage(char *hostIP,u_short hostport,char * buffer);
 
+/*
+    This function receives a UDP message on the passed port then
+    places the message in the buffer, as well as prints it to console
+*/
 int receiveMessage(u_short port,char *buffer);
 
 
